d06b420aebb5d22dfce2f79492b9d53e219ee61c
zachblume <zachblume@gmail.com>
2023-02-04 01:46:47 -0500

    problem 22

---
+++ 22generateParentheses.js
@@ -0,0 +1,10 @@
+/**
+ * 22. Generate Parentheses
+ * Given n pairs of parentheses, write a function to generate all
+ * combinations of well-formed parentheses.
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    
+};
