2c9db3d81a5d1cf3515635485e963e080e79cf52
Zach Blume <zachblume@gmail.com>
2024-10-18 17:56:59 -0400

    without regex

--- 2696MinimumStringLengthAfterRemovingSubstrings.js
+++ 2696MinimumStringLengthAfterRemovingSubstrings.js
@@ -1,5 +1,5 @@
 // 2696. Minimum String Length After Removing Substrings
-function minLength(s: string): number {
+function minLength(s) {
   // You are given a string s consisting only of uppercase English letters.
   // You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
   // Return the minimum possible length of the resulting string that you can obtain.
@@ -8,9 +8,20 @@ function minLength(s: string): number {
   // Because they never overlap, theres no point in choosing to not remove it.
   // So you must always remove it when you find it.
   // So we can just iterate through backwards, removing them as wel go, and return remaining len
-  let currentString = s
-  while(currentString != currentString.replaceAll(/(AB|CD)/g,"")){
-      currentString = currentString.replaceAll(/(AB|CD)/g,"")
+  // let currentString = s
+  // while(currentString != currentString.replaceAll(/(AB|CD)/g,"")){
+  //   currentString = currentString.replaceAll(/(AB|CD)/g,"")
+  // }
+  // return currentString.length
+  let currentString = s;
+  while (currentString.indexOf("AB") !== -1 || currentString.indexOf("CD") !== -1) {
+    for (let i = 0; i < currentString.length - 1; i++) {
+      const char = currentString[i] + currentString[i + 1];
+      if (["AB", "CD"].includes(char)) {
+        currentString = currentString.slice(0, i) + currentString.slice(i + 2);
+        i -= 2;
+      }
+    }
   }
-  return currentString.length
-};
+  return currentString.length;
+}
